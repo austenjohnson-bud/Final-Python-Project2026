a69477804a5d03b606d6d75d1268a6b2b9150ae7
zajohns1 <zajohns1@hatboro-horsham.org>
2026-05-07 14:11:48 -0400

    Finished character stats with JSON

--- Main.py
+++ Main.py
@@ -15,16 +15,20 @@ class Character:
         self.move2pow = move2pow
         self.move3pow = move3pow
 
-
+stats = ["hp", "attack", "defence"]
 character = ["Torch Head", "Mr. Frost", "Lightning Rod", "Vine Master"]
 for x in character:
     print(x)
 Character1 = Character("TorchHead", 100, 25, 0, 'FireBlast', 'FirePunch', 'FlareBomb', 30, 35, 28 )
 Character2 = Character("Mr. Frost", 100, 25, 0, 'FrostBreath', 'FrostPunch', 'Icicle', 29,  35, 30)
+Character3 = Character("Lightning Rod", 100, 25, 0, 'Thundershock', 'ThunderPunch', 'Shockwave', 30, 36, 28)
+Character4 = Character("Vine Master", 100, 25, 0, 'Grapple Vine', 'Life Drain', 'Thorn Attack', 28, 10, 32)
+possible_characters = [Character1, Character2, Character3, Character4]
 chosen = input("Select your character")
-for x in character:
-    if chosen == x:
-        chosen = x
+myCharacter = Character1
+for x in possible_characters:
+    if chosen == x.name:
+        myCharacter = x
 enemy = input("Chose your opponent")
 for x in character:
     if enemy == x:
@@ -36,7 +40,11 @@ if chosenAction == "Attack":
     input("What will you use?")
 elif chosenAction == "Items":
     Items = ["Potion", "Pow Up", "Def Up"]
-    input("Select Items")
+    chosenItem = input("Select Items")
+    if chosenItem == "Pow Up":
+        myCharacter.hp += 5
 elif chosenAction == "Run":
     print("You ran away")
+
+
 
